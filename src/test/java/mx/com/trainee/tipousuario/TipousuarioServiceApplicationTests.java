e11a5ef64bc573ccfd16b77fc83c07cf4c1b698e
Traineer <luis.sanmartz@gmail.com>
2024-04-11 01:58:19 -0600

    baseline of the project

---
+++ src/test/java/mx/com/trainee/tipousuario/TipousuarioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.trainee.tipousuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipousuarioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
